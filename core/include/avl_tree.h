0dec241e28d5ec8a917e42649f8ce17c824a6783
Fer Soriano <fer8f10@hotmail.com>
2026-03-08 21:41:29 -0600

    Implement AVL tree data structure with node creation, insertion, and traversal functions

---
+++ core/include/avl_tree.h
@@ -0,0 +1,29 @@
+#ifndef ALV_TREE_H
+#define AVL_TREE_H
+
+typedef void (*Callback)(int id, const char* name, const char* author);
+
+typedef struct Node{
+    int id;
+    char* name;
+    char* author;
+    int height;
+    struct Node* left;
+    struct Node* right;
+} Node;
+
+
+Node* createNode(int id, char* name, char* author);
+Node* insertNode(Node* root, Node* n);
+
+void inorden(Node* root, Callback callback);
+void preorden(Node* root, Callback callback);
+
+// helpers
+static int getHeight(Node* n);
+static void setHeight(Node** n);
+static int max(int a, int b);
+static Node* leftRotation(Node* n);
+static Node* rightRotation(Node* n);
+
+#endif
