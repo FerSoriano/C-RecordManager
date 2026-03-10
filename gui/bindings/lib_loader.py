5b1a9d745740af4173a1bb27d52361acb65b259e
FerSoriano <fer8f10@hotmail.com>
2026-03-09 20:13:27 -0600

    Refactor code for improved readability and consistency in app.py, avl_tree.py, and lib_loader.py

--- gui/bindings/lib_loader.py
+++ gui/bindings/lib_loader.py
@@ -2,6 +2,6 @@ import ctypes
 import os
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
-lib_path = os.path.join(base_dir,"..","..", "lib", "libcore.so")
+lib_path = os.path.join(base_dir, "..", "..", "lib", "libcore.so")
 
 c_lib = ctypes.CDLL(lib_path)
